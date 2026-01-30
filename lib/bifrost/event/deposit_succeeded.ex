07969faf596b8ecfab94de2cbf87c2c4cca95b2a
Rafael Willians <me@rwillians.com>
2026-01-30 16:03:46 -0300

    amend

--- lib/bifrost/event/deposit_succeeded.ex
+++ lib/bifrost/event/deposit_succeeded.ex
@@ -11,5 +11,11 @@ defmodule Bifrost.Event.DepositSucceeded do
            provider_fee: Zc.money(:cents),
            platform_pricing_percentage: Zc.percentage(),
            platform_pricing_fixed_amount: Zc.money(:cents),
-           platform_fee: Zc.money(:cents)
+           platform_fee: Zc.money(:cents),
+           #   info that was already sent in previous events but
+           # ↓ that's needed again to create the wallet transactions
+           more: Z.strict_map(%{
+             provider_id: Zc.non_empty_string(),
+             currency: Zc.currency()
+           })
 end
