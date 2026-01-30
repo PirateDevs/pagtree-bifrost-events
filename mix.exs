e397a807b6b84737ed37526f1eadc4a1a47af768
Rafael Willians <me@rwillians.com>
2026-01-30 10:55:55 -0300

    v1.2.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BifrostCommons.MixProject do
   def project do
     [
       app: :bifrost_events,
-      version: "1.2.1",
+      version: "1.2.3",
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()
